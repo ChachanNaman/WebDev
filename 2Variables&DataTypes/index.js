06fce5e9d72da289c334548e78a6f38dc22c2d88
Naman <chachannaman@gmail.com>
2025-08-11 19:54:11 +0530

    js start

---
+++ 2Variables&DataTypes/index.js
@@ -0,0 +1,50 @@
+// var age = 25;
+
+// if(true){
+//     console.log(age);
+// }
+
+function solve(){
+    var age = 25;
+    console.log(age);
+}
+solve();
+// console.log(age); 
+// will throw error as age not defined coz called outside the function
+
+{
+let a = 10;
+console.log(a);
+}
+// console.log(a); //cant use as its outside the block
+
+//Reassignment -> possible
+let y = 30;
+console.log(y);
+
+y = "naman"
+console.log(y);
+
+//Redefinition -> not possible
+// let j = 40;
+
+// let j = "mayank"
+// console.log(j);
+
+const d = 28;
+console.log(d);
+
+// d = 29;
+// console.log(d);
+
+//DataTypes : 
+let value = 1739871238284104810581395835;
+console.log(value);
+console.log(typeof(value));
+
+value=false;
+console.log(typeof(value));
+
+
+
+
