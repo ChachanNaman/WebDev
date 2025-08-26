d096192a6e3f059489759a9d086ca72a234afca0
Naman <chachannaman@gmail.com>
2025-08-27 03:02:56 +0530

    dom1 done

---
+++ 12JSDOM/index.js
@@ -0,0 +1,11 @@
+// inserting element code
+// let mydiv = document.querySelector('#mydiv');
+// let newele = document.createElement('span');
+// newele.textContent = "kaise ho sab ji";
+// mydiv.insertAdjacentElement('beforebegin', newele);
+
+//removing child code
+//removing para from div 
+let mydiv = document.querySelector("#mydiv");
+let child = document.querySelector("#fpara");
+mydiv.removeChild(child);
